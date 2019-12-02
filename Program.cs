910c4e833f33d433780100d1e3674388b6fdd3ea
amdiag <amdiag@ya.ru>
2019-12-02 23:20:00 +0300

    Updated code. Refactoring

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
@@ -7,87 +6,84 @@ using System.Threading;
 
 class TimerMonitor
 {
-    private const int millsInSec = 1000;
+    private const int MillsInSec = 1000;
     static void Main(string[] args)
     {
-        int KillTime = new int();
-        int RefreshTime = new int();
+        int killTime = 0;
+        int refreshTime = 0;
         if (args.Length != 3)
         {
-           printUsage();
+           PrintUsage();
            return; 
         }
-        if ( (!string.IsNullOrEmpty(args[0])) && 
-            int.TryParse(args[1], out KillTime) &&            
-            int.TryParse(args[2], out RefreshTime))
-        {            
+        if ((!string.IsNullOrEmpty(args[0])) &&
+            int.TryParse(args[1], out killTime) &&
+            int.TryParse(args[2], out refreshTime))
+        {
             var autoEvent = new AutoResetEvent(false);
-            //
-            var statusChecker = new StatusChecker( args[0], KillTime);
-            var stateTimer = new Timer(statusChecker.StartProcessMonitor,
-                                       autoEvent, 0, RefreshTime* millsInSec);
-            autoEvent.WaitOne();
+            var statusChecker = new StatusChecker(args[0], killTime);
+            using (var stateTimer = new Timer(statusChecker.StartProcessMonitor, autoEvent, 0, refreshTime * MillsInSec))
+            {
+                autoEvent.WaitOne();
+            }
         }
-        else{
-            Console.WriteLine(args[0]+" "+KillTime+" "+RefreshTime);
-            printUsage();
+        else
+        {
+            Console.WriteLine(args[0] + " " + killTime + " " + refreshTime);
+            PrintUsage();
             return;
         }
     }
 
-    private static void printUsage()
+    private static void PrintUsage()
     {
-        Console.WriteLine("Usage: monitor <process name> <time limit> <refresh time>");
+        Console.WriteLine("Usage: monitor <process name> <time limit> <refresh time>.");
     }
 }
 
 
 class StatusChecker
 {
-    private string processName;
-    private int KillTime;
+    private readonly string _processName;
+    private readonly int _killTime;
 
     public StatusChecker(string name, int time)
     {
-        processName = name;
-        KillTime = time;
+        _processName = name;
+        _killTime = time;
     }
 
-    public void StartProcessMonitor(Object stateInfo)
+    public void StartProcessMonitor(object stateInfo)
     {
-        ArrayList listToKill = FindProcess(Process.GetProcessesByName(processName));
-        if (listToKill.Count != 0)
+        var processesToKill = FindProcess(Process.GetProcessesByName(_processName));
+        if (processesToKill.Any())
         {
-            foreach (Process proc in listToKill)
+            foreach (Process proc in processesToKill)
             {
-                this.KillProcess(proc);
-            }
-
-        }
-    }
-
-    private ArrayList FindProcess(Process[] listProcesses)
-    {
-        ArrayList listToKill = new ArrayList();
-        if (listProcesses.Any())
-        {
-            foreach (Process proc in listProcesses)
-            {
-                DateTime ProcessTime = proc.StartTime;
-                TimeSpan duration = DateTime.Now - ProcessTime;
-
-                if (duration.TotalMinutes > KillTime)
-                    listToKill.Add(proc);
+                KillProcess(proc);
             }
         }
-        return listToKill;
     }
 
-    private bool KillProcess(Process process)
+    private Process[] FindProcess(Process[] processes)
     {
-        process.Kill();
-        while (!process.HasExited) { }
-        Console.WriteLine("Process killed. Info: "+process.Id+" "+process.ProcessName+". Started time "+process.StartTime.ToString());
-        return true;
+        if (processes.Any())
+        {
+            return processes.Where(x => (DateTime.Now - x.StartTime).TotalMinutes > _killTime).ToArray();
+        }
+        return new Process[0];
+    }
+
+    private void KillProcess(Process process)
+    {
+        try
+        {
+            process.Kill();
+            Console.WriteLine($"Process killed. Info: {process.Id} {process.ProcessName}. Started time {process.StartTime.ToLongDateString()}.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error occured. Info: {process.Id} {process.ProcessName} {Environment.NewLine} {e.Message}.");
+        }
     }
 }
